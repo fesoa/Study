ddee8419cb755b3402610f36bf215aa2f510c642
fesoa <fernandofesoa@gmail.com>
2019-02-12 16:26:38 +0900

    네비게이션 가드 추가

--- vue-trello/vue-login/src/router/index.js
+++ vue-trello/vue-login/src/router/index.js
@@ -8,8 +8,14 @@ import NotFound from '../components/NotFound.vue'
 
 Vue.use(VueRouter)
 
+const requireAuth = (to, from, next) =>{        // 네비게이션 가드 사용
+    const isAuth = localStorage.getItem('token')    //로컬스토리지에 토큰이 있는가?
+    const loginPath = `/login?rPath=${encodeURIComponent(to.path)}`
+    isAuth ? next() : next(loginPath)
+}
+
 const routes = [
-    { path: '/', component: Home },
+    { path: '/', component: Home, beforeEnter: requireAuth },
     { path: '/login', component: Login},
     { path: '/b/:bid', component: Board, children: [        //Board컴포넌트에서 Card 출력 해줘야함
         { path: 'c/:cid', component: Card }
